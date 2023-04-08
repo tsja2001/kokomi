46b76ff0befe7556d093ac76d28797b62fa4e9f8
tsja2001 <2945532754@qq.com>
2023-04-08 13:15:03 +0800

    feat: 修改标题

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,10 +17,10 @@ try {
 }
 
 export const metadata = {
-  title: "ChatGPT Next Web",
+  title: "Kokomi-GPT",
   description: "Your personal ChatGPT Chat Bot.",
   appleWebApp: {
-    title: "ChatGPT Next Web",
+    title: "Kokomi-GPT",
     statusBarStyle: "default",
   },
   themeColor: "#fafafa",
