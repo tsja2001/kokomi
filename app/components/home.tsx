ff817331feb5c0b8d0edfd895324cc5ba58b7a7d
tsja2001 <2945532754@qq.com>
2023-04-08 13:14:05 +0800

    feat: 修改主题

--- app/components/home.tsx
+++ app/components/home.tsx
@@ -119,9 +119,9 @@ function _Home() {
         className={styles.sidebar + ` ${showSideBar && styles["sidebar-show"]}`}
       >
         <div className={styles["sidebar-header"]}>
-          <div className={styles["sidebar-title"]}>ChatGPT Next</div>
+          <div className={styles["sidebar-title"]}>深海的加护</div>
           <div className={styles["sidebar-sub-title"]}>
-            Build your own AI assistant.
+            来和Kokomi-GPT聊天吧
           </div>
           <div className={styles["sidebar-logo"]}>
             <ChatGptIcon />
