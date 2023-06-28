e0084f5e31ffe094ab19718a9311fc784eb00fe8
tsja2001 <52786405+tsja2001@users.noreply.github.com>
2023-06-28 16:25:07 +0800

    feat: 更新版本

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,8 +5,8 @@ import "./styles/highlight.scss";
 import { getClientConfig } from "./config/client";
 
 export const metadata = {
-  title: "ChatGPT Next Web",
-  description: "Your personal ChatGPT Chat Bot.",
+  title: "Kokomi GPT",
+  description: "来喝Kokomi聊天吧",
   viewport: {
     width: "device-width",
     initialScale: 1,
@@ -17,7 +17,7 @@ export const metadata = {
     { media: "(prefers-color-scheme: dark)", color: "#151515" },
   ],
   appleWebApp: {
-    title: "ChatGPT Next Web",
+    title: "Kokomi GPT",
     statusBarStyle: "default",
   },
 };
